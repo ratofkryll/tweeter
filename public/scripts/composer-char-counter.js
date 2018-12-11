ef8834e8cf52bcb07637394ae0b5b94f1688610b
Gioanna Rheumer <ratofkryll@gmail.com>
2018-12-11 23:59:52 +0000

    minor refactor to composer-char-counter.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,11 +4,12 @@ $(document).ready(function() {
   console.log('Doc is ready!');
 
   $('.new-tweet textarea').on('keyup', function (event) {
-    const textInput = $(this).val();
+    const textInput = $(this);
+    const charCount = textInput.val();
     const counter = $(this).siblings('.counter');
 
-    counter.text(140 - textInput.length);
-    counter.toggleClass('counter-negative-value', textInput.length > 140);
+    counter.text(140 - charCount.length);
+    counter.toggleClass('counter-negative-value', charCount.length > 140);
   });
 
 });
