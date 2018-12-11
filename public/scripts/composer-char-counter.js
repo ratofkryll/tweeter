66e4914281a9103bbd2e204848e31e0b08285486
Gioanna Rheumer <ratofkryll@gmail.com>
2018-12-11 22:42:16 +0000

    added val length of textarea

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,9 +3,9 @@ console.log('Hi!');
 $(document).ready(function() {
   console.log('Doc is ready!');
 
-  $('.new-tweet textarea').on('keypress', function (event) {
-    console.log(this);
-    
+  $('.new-tweet textarea').on('keyup', function (event) {
+    const textInput = $(this).val();
+    console.log(140 - textInput.length);
   });
 
 });
