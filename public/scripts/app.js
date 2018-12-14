8e25a41076ad4507ded6a7f667297eaaa47687b4
Gioanna Rheumer <ratofkryll@gmail.com>
2018-12-14 00:31:59 +0000

    replaced icon images with fontAwesome icons & updated app.js to CS6

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(() => {
 
   // Create & render tweet html
   function renderTweets (tweetData) {
@@ -28,9 +28,11 @@ $(document).ready(function() {
 
     $tweetFooter.append(
       `<span class="posted-time">${moment(tweet.created_at).fromNow()}</span>
-      <img class="icon" src="/images/heart.png" alt="Heart icon" />
-      <img class="icon" src="/images/refresh.png" alt="Refresh icon" />
-      <img class="icon" src="/images/flag.png" alt="Flag icon" />`
+      <div class="icons">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>`
     );
 
     $tweetContainer.append($tweetHeader, $tweetContent, $tweetFooter);
@@ -39,32 +41,32 @@ $(document).ready(function() {
   }
 
   // Handle tweet data GET
-  $.getJSON('/tweets', function (data) {
+  $.getJSON('/tweets', (data) => {
     renderTweets(data);
   });
 
   // Handle new tweet POST
-  $('#compose-tweet').submit(function (e) {
+  $('#compose-tweet').submit((e) => {
     e.preventDefault();
 
     const $charCount = $('#new-tweet textarea').val().length;
     const $error = $('.error-message');
 
     if ($error.height() > 0) {
-      $error.slideUp('fast', function () {
+      $error.slideUp('fast', () => {
         $error.text('');
       });
     }
 
     if ($charCount === 0 || $charCount === ' ') {
-      $error.slideDown('fast', function () {
+      $error.slideDown('fast', () => {
         $error.text('Please enter up to 140 characters.');
       });
       return;
     }
 
     if ($charCount > 140) {
-      $error.slideDown('fast', function () {
+      $error.slideDown('fast', () => {
         $error.text('Your tweet must be less than 140 characters.');
       });
       return;
@@ -72,11 +74,11 @@ $(document).ready(function() {
 
     const $postTweet = $.post('/tweets', $(this).serialize());
 
-    $postTweet.done(function (data) {
+    $postTweet.done((data) => {
       $('#new-tweet textarea').val('');
       $('#new-tweet .counter').text('140');
 
-      $.getJSON('/tweets', function (data) {
+      $.getJSON('/tweets', (data) => {
         $('#tweets-wrapper').empty();
         renderTweets(data);
       });
@@ -84,14 +86,8 @@ $(document).ready(function() {
   });
 
   // Show/hide tweet submission form & nav button behaviour
-  $('.compose').hover(function () {
-    $('.icon-hover').attr('src', '/images/nav/compose-icon-bw.png');
-  }, function() {
-    $('.icon-hover').attr('src', '/images/nav/compose-icon-green.png');
-  });
-
-  $('.compose').click(function () {
-    $('#new-tweet').slideToggle('fast', function () {
+  $('.compose').click(() => {
+    $('#new-tweet').slideToggle('fast', () => {
       $('#new-tweet textarea').focus();
     });
   });
